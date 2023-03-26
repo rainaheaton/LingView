f493ea17cb3190e88cf821ff1db9f1044e34c190
rainaheaton <126720782+rainaheaton@users.noreply.github.com>
2023-03-26 08:23:27 -0500

    Update AboutPage.jsx

--- jsx/App/AboutPage.jsx
+++ jsx/App/AboutPage.jsx
@@ -6,12 +6,12 @@ const aboutPageJSX = {
   [ENGLISH]:
     <div>
       <p>This is display tool for the oral histories collected as part of the Enenlhet Language Documentation Project with the (Toba) Enenlhet communities in Pozo Amarillo and Casanillo, Paraguay. The goal here is that speakers can interact with the texts/recordings, enjoy them, and provide feedback to the project team. To explore all the files from the project, see the archival repository at the University of Texas at Austin: https://ailla.utexas.org/es/islandora/object/ailla%3A266557 </p>
-      <p>To customize this welcome text, edit the jsx/App/AboutPage.jsx file, then run webpack for your changes to take effect. </p>
+      <p> </p>
     </div>,
   [ESPANOL]:
     <div>
       <p>Esta es una herramienta de exhibición de las historias orales recopiladas como parte del Proyecto de Documentación de la Lengua Enenlhet con las comunidades (Toba) Enenlhet en Pozo Amarillo y Casanillo, Paraguay. El objetivo aquí es que los oradores puedan interactuar con los textos/grabaciones, disfrutarlos y brindar retroalimentación al equipo del proyecto. Para explorar todos los archivos del proyecto, consulte el repositorio de archivos de la Universidad de Texas en Austin: https://ailla.utexas.org/es/islandora/object/ailla%3A266557 </p>
-      <p>Es una prueba </p>
+      <p> </p>
     </div>,
   [FRANCAIS]:
     <div>
